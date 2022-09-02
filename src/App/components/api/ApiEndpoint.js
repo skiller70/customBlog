1ac09dfed78ec0a4480333d8f02a7bf68eb1af5a
skiller70 <singhlucky007007@gmail.com>
2022-09-02 20:28:28 +0530

    added endpoint

--- src/App/components/api/ApiEndpoint.js
+++ src/App/components/api/ApiEndpoint.js
@@ -3,7 +3,7 @@
 //     baseURL : "http://localhost:4000/blog"
 // }) ; 
 
-const MAIN_END_POINT = "http://localhost:4000/blog"
+const MAIN_END_POINT = "https://luckyskiller.herokuapp.com/blog"
 
 export const REGISTER_END_POINT = `${MAIN_END_POINT}/register`;
     
