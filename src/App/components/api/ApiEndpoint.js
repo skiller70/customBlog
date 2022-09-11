a5033c3507826fffc54a626fbb2affaa62012dce
skiller70 <singhlucky007007@gmail.com>
2022-09-11 21:04:39 +0530

    added apiendpoint

--- src/App/components/api/ApiEndpoint.js
+++ src/App/components/api/ApiEndpoint.js
@@ -4,7 +4,7 @@
 // }) ; 
 
 
-export const MAIN_END_POINT = "http://localhost:4000/blog"
+export const MAIN_END_POINT = "https://luckyskiller.herokuapp.com/blog"
 
 export const REGISTER_END_POINT = `${MAIN_END_POINT}/register`;
     
